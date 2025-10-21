bdc239ddd9a8084ee39eaab603f63db506640e36
prakash <prakash.mohan@nrel.gov>
2025-10-21 17:42:25 -0600

    add code from 2016

---
+++ ke.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy import interpolate as interp
+import matplotlib.pyplot as plt
+
+def read_data(Re):
+    data = {}
+    file1 = np.loadtxt('data/LM_Channel_%d_mean_prof.dat'%Re,comments='%')
+    file2 = np.loadtxt('data/LM_Channel_%d_vel_fluc_prof.dat'%Re,comments='%')
+    data['Y']     = file1[:,0]
+    data['U']     = file1[:,2]
+    data['dUdy']  = file1[:,3]
+    data['uv']    = file2[:,5]
+    return data
---
+++ rans.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.integrate import ode
+import matplotlib.pyplot as plt
+import numpy.linalg as LA
+
+from sa import *
+# from sa_full import *
+
+#state:= [u, nu_tilda]^T
+def rhs(t, state):
+    return get_dXdt(state)
+
+def time_march(initial_state, steps, dt ):
+    integrator = ode(rhs).set_integrator('lsoda')
+    integrator.set_initial_value(initial_state, 0)
+    states = np.empty((steps+1,2*N))
+    i = 0
+    states[0,:] = initial_state
+    while integrator.successful() and i < steps:
+        i+=1
+        states[i,:] = integrator.integrate(integrator.t+dt)
+        print(i, LA.norm( get_dXdt(states[i,:] ) ))
+        
+        if( i % 10 is 0):
+            np.save('%d-last'%Re_tau,states[i,:])
+    return states
