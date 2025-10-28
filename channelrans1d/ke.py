6b8bd943d9abd334247bae5d83880d24076f2eeb
prakash <prakash.mohan@nrel.gov>
2025-10-28 12:04:14 -0600

    No need to subsample the data

--- channelrans1d/ke.py
+++ channelrans1d/ke.py
@@ -8,11 +8,11 @@ def read_data(Re):
     file1 = np.loadtxt(
         os.path.join(CHANNELRANS1D_DNS_DATA, f"LM_Channel_{Re}_mean_prof.dat"),
         comments="%",
-    )[::2]
+    )
     file2 = np.loadtxt(
         os.path.join(CHANNELRANS1D_DNS_DATA, f"LM_Channel_{Re}_vel_fluc_prof.dat"),
         comments="%",
-    )[::2]
+    )
     data["Y"] = file1[:, 0]
     data["U"] = file1[:, 2]
     data["dUdy"] = file1[:, 3]
